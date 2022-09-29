2a51b85b55cf27e668109b928e1cc759b0a32203
Lisa_Lyakh <99537140+LisaLyakh@users.noreply.github.com>
2022-09-29 13:47:30 +0300

    add team

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+    aboutDev: document.querySelector('.footer-btn'),
+    teamModalBtnClose: document.querySelector('.close-btn'),
+    backdrop: document.querySelector('.backdrop'),
+};
