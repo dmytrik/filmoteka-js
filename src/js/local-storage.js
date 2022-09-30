5281398e2eb8a0d874548bfbe8fe372df319ee77
Samoylenko Dmitry <107771156+dmytrik@users.noreply.github.com>
2022-09-30 18:49:36 +0300

    add

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,56 +1,57 @@
-const modalRef = document.querySelector('.modal');
+const modalRef = document.querySelector('.modal_film');
 
-modalRef.addEventListener("click", addFilmIdToLocalStorage);
-
+modalRef.addEventListener('click', addFilmIdToLocalStorage);
 
 const STORAGE_KEY_WATCHED = 'watched-films-id';
 const STORAGE_KEY_QUEUE = 'queue-films-id';
 
 let watchedLocalStorageIdArr = [];
-let queueLocalStorageIdArr = [];   
-  
+let queueLocalStorageIdArr = [];
+
 function addFilmIdToLocalStorage(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    addToWatchedArr(event);
+  addToWatchedArr(event);
 
-    addQueueArr(event);
-    
+  addQueueArr(event);
 }
 
 function addToWatchedArr(event) {
-    const id = event.target.dataset.id;
-    if (event.target.textContent === "add to watched") {
-        if (watchedLocalStorageIdArr.includes(id)) {
-            return
-        }
-        watchedLocalStorageIdArr.push(id);
-        console.log(watchedLocalStorageIdArr);
-        changeTextWatchedBtn(event);
-        localStorage.setItem("STORAGE_KEY_WATCHED", JSON.stringify(watchedLocalStorageIdArr));
+  const id = event.target.dataset.id;
+  console.log(event.target.textContent);
+  if (event.target.textContent === 'add to watched') {
+    if (watchedLocalStorageIdArr.includes(id)) {
+      return;
     }
+    watchedLocalStorageIdArr.push(id);
+    console.log(watchedLocalStorageIdArr);
+    changeTextWatchedBtn(event);
+    localStorage.setItem(
+      'STORAGE_KEY_WATCHED',
+      JSON.stringify(watchedLocalStorageIdArr)
+    );
+  }
 }
 
 function addQueueArr(event) {
-    const id = event.target.dataset.id;
-    if (event.target.textContent === "add to queue") {
-        if (queueLocalStorageIdArr.includes(id)) {
-            return
-        }
-        addedStyleToQueue(event)
-        queueLocalStorageIdArr.push(id);
-        localStorage.setItem("STORAGE_KEY_QUEUE", JSON.stringify(queueLocalStorageIdArr));
+  const id = event.target.dataset.id;
+  if (event.target.textContent === 'add to queue') {
+    if (queueLocalStorageIdArr.includes(id)) {
+      return;
     }
+    addedStyleToQueue(event);
+    queueLocalStorageIdArr.push(id);
+    localStorage.setItem(
+      'STORAGE_KEY_QUEUE',
+      JSON.stringify(queueLocalStorageIdArr)
+    );
+  }
 }
 
-
-
-
 function changeTextWatchedBtn(event) {
-    event.target.textContent = "remove from watched"; 
+  event.target.textContent = 'remove from watched';
 }
 
 function addedStyleToQueue(event) {
-  
   event.target.textContent = 'remove from queue';
 }
