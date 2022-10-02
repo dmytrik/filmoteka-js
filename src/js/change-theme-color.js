b3545b4ee28be67aec3d8697a764dc0ea99b1c24
OlehStarko <starartds@gmail.com>
2022-10-02 21:49:53 +0300

    edit-html

---
+++ src/js/change-theme-color.js
@@ -0,0 +1,59 @@
+const changeColorButton = document.querySelector('.change__bg-color');
+// console.log(changeColorButton);
+const bodyTheme = document.querySelector('body');
+const toogleEl = document.querySelector('.slider');
+const headerTheme = document.querySelector('.header');
+// const headerThemeLibrary = document.querySelector('.header-library');
+const headerBackground = document.querySelector('.header__container-index');
+// const imgHover = document.querySelector('.films__list');
+
+function darkTheme() {
+  bodyTheme.classList.add('dark-theme');
+  headerTheme.classList.add('dark-theme');
+  // imgHover.classList.add('dark-theme');
+  headerBackground.classList.add('dark-theme');
+  bodyTheme.classList.remove('blue-theme');
+  headerTheme.classList.remove('blue-theme');
+  headerBackground.classList.remove('blue-theme');
+  // imgHover.classList.remove('blue-theme');
+}
+
+function blueTheme() {
+  // imgHover.classList.add('blue-theme');
+  bodyTheme.classList.add('blue-theme');
+  headerTheme.classList.add('blue-theme');
+  headerBackground.classList.add('blue-theme');
+  bodyTheme.classList.remove('dark-theme');
+  headerTheme.classList.remove('dark-theme');
+  headerBackground.classList.remove('dark-theme');
+  // imgHover.classList.remove('dark-theme');
+}
+
+changeColorButton.addEventListener('click', buttonClick);
+
+function buttonClick() {
+  if (bodyTheme.classList.contains('dark-theme')) {
+    blueTheme();
+    localStorage.setItem('theme', bodyTheme.classList);
+  } else {
+    darkTheme();
+    localStorage.setItem('theme', bodyTheme.classList);
+  }
+}
+
+export { changeColorButton, bodyTheme, buttonClick };
+const avtiveTheme = localStorage.getItem('theme');
+toogleChecked();
+updateTheme();
+
+function toogleChecked() {
+  if (avtiveTheme === 'blue-theme') {
+    changeColorButton.setAttribute('checked', true);
+  }
+}
+
+function updateTheme() {
+  if (avtiveTheme === 'blue-theme') {
+    blueTheme();
+  }
+}
